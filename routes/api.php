ee9a48d840380922a3970cd97082dcfc8101c676
karthi282k <karthigaiselvan28062000@gmail.com>
2024-05-20 23:49:41 +0530

    sanctum auth added

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/register', [App\Http\Controllers\Api\AuthController::class, 'createUser']);
+Route::post('/auth/login', [App\Http\Controllers\Api\AuthController::class, 'loginUser']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    //here you can add all your api routes
 });
