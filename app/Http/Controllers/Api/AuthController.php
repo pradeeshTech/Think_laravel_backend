3e6e5e0ed9f0179381b039c2405dcff5838da798
VishnuV2410 <vvishva185@gmail.com>
2024-05-31 23:20:29 +0530

    testing

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -105,6 +105,7 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         // Revoke the token that was used to authenticate the current request
+        // 'testing'
         $request->user()->currentAccessToken()->delete();
 
         return response()->json(['message' => 'Logged out successfully.'], 200);
